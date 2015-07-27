ac607c043e8bcf250b8bfb1a93844468b2f7bd75
Gary Teh <garyjob@Garys-MacBook-Air.local>
2015-07-26 19:04:07 -0700

    updated the redis server location

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['KRAKE_REDIS_HOST']}:#{ENV['KRAKE_REDIS_PORT']}/0" }
+  config.redis = { url: "redis://#{ENV['RN_REDIS_HOST']}:#{ENV['RN_REDIS_PORT']}/0" }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['KRAKE_REDIS_HOST']}:#{ENV['KRAKE_REDIS_PORT']}/0" }
+  config.redis = { url: "redis://#{ENV['RN_REDIS_HOST']}:#{ENV['RN_REDIS_PORT']}/0" }
 end
