b207656ebd5a82dfcccb06a1beb7f49f4c1b5144
Gary Teh <garyjob@Garys-MacBook-Air.local>
2015-07-26 19:01:05 -0700

    redeployment script inclusion

---
+++ redeploy.sh
@@ -0,0 +1,21 @@
+git pull origin master
+
+bundle install
+
+rake db:migrate
+
+RAILS_ENV=production bundle exec rake assets:precompile
+
+ps aux | awk '/rails s.*-p 3002/ { print $2 }' | xargs kill
+nohup bundle exec rails s -e production -p 3002 > log/production.log 2>&1 &
+
+ps aux | awk '/sidekiq(.*)krake_ror/ { print $2 }' | xargs kill
+bundle exec sidekiq -e production -C config/sidekiq.yml -P tmp/pids/sidekiq.pid  -d
+
+# To be restart manually if and when required
+# kill -s 9 PID_OF_FOREMAN PROCESS - ps aux | grep foreman
+# nohup foreman start > log/sidekiq.log 2>&1 &
+
+# To run the console from the bundle
+# Command:
+#   bundle console
