56cd07785ff749ab47eb1f9347fb49ec234d3aa8
Gary Teh <garyjob@Garys-MacBook-Air.local>
2015-09-26 23:37:33 -0700

    publish to slack each time a new job is created

--- app/models/import_job.rb
+++ app/models/import_job.rb
@@ -1,4 +1,10 @@
 class ImportJob < ActiveRecord::Base
   has_many :import_diffs, dependent: :destroy
   has_many :import_logs, dependent: :destroy
+
+  after_commit :slack_it
+
+  def slack_it
+    SlackPinger.perform_async id
+  end
 end
