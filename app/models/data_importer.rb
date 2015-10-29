3d331e8927eda4e4c8b533e084e5541ea4773622
Gary Teh <garyjob@unknown1040f37bfc8e.attlocal.net>
2015-10-29 02:19:53 -0700

    ensure we do not mess up the date again

--- app/models/data_importer.rb
+++ app/models/data_importer.rb
@@ -115,7 +115,6 @@ class DataImporter
       row["origin_url"] = row["apartment page"]
       row["import_job_id"] = job.id
       row["sqft"] = row["size"]
-      row["event_date"]       = ImportFormatter.to_date_short_year row["event_date"]      
 
       case row["event_name"]
       when "Listed for rent"
