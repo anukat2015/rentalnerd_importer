992f720648ee3af1c276fd562c211a897008b9cd
Gary Teh <garyjob@Garys-MacBook-Air.local>
2015-12-10 23:44:06 -0800

    ensures we only activate a prediction model after its been fully imported

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207062723) do
+ActiveRecord::Schema.define(version: 20151211073951) do
 
   create_table "covariances", force: true do |t|
     t.integer  "prediction_model_id"
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(version: 20151207062723) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "neighborhood_id"
-    t.boolean  "active",                                        default: true
+    t.boolean  "active",                                        default: false
     t.decimal  "regular_coefficient", precision: 30, scale: 20
     t.decimal  "luxury_coefficient",  precision: 30, scale: 20
   end
