ee1e81fd3a2d00d11df9d7677ab5f56fa490a9cf
Ilya Voytov <ilya@voytov.com>
2015-12-12 11:57:40 -0800

    reverted the schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211212454) do
-
-  create_table "covariances", force: true do |t|
-    t.integer  "prediction_model_id"
-    t.string   "row_type"
-    t.integer  "row_neighborhood_id"
-    t.integer  "row_year"
-    t.boolean  "row_is_luxurious"
-    t.string   "col_type"
-    t.integer  "col_neighborhood_id"
-    t.integer  "col_year"
-    t.boolean  "col_is_luxurious"
-    t.decimal  "coefficient",         precision: 30, scale: 20
-    t.string   "row_raw"
-    t.string   "col_raw"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20150919184617) do
 
   create_table "import_diffs", force: true do |t|
     t.text     "address"
@@ -48,11 +31,6 @@ ActiveRecord::Schema.define(version: 20151211212454) do
     t.integer  "new_log_id"
     t.integer  "import_job_id"
     t.string   "transaction_type", default: "rental"
-    t.date     "date_transacted"
-    t.boolean  "garage"
-    t.integer  "year_built"
-    t.integer  "level",            default: 1
-    t.boolean  "sfh",              default: false
   end
 
   create_table "import_jobs", force: true do |t|
@@ -76,17 +54,6 @@ ActiveRecord::Schema.define(version: 20151211212454) do
     t.datetime "updated_at"
     t.integer  "import_job_id"
     t.string   "transaction_type", default: "rental"
-    t.date     "date_transacted"
-    t.boolean  "garage"
-    t.integer  "year_built"
-    t.integer  "level",            default: 1
-    t.boolean  "sfh",              default: false
-  end
-
-  create_table "luxury_addresses", force: true do |t|
-    t.string   "address"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "neighborhood_vertices", force: true do |t|
@@ -109,48 +76,22 @@ ActiveRecord::Schema.define(version: 20151211212454) do
     t.datetime "updated_at"
   end
 
-  create_table "park_vertices", force: true do |t|
-    t.integer  "park_id"
-    t.integer  "vertex_order"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "parks", force: true do |t|
-    t.string   "name"
-    t.integer  "size"
-    t.string   "shapefile_source"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "prediction_models", force: true do |t|
-    t.decimal  "base_rent",                precision: 30, scale: 20
-    t.decimal  "bedroom_coefficient",      precision: 30, scale: 20
-    t.decimal  "bathroom_coefficient",     precision: 30, scale: 20
-    t.decimal  "elevation_coefficient",    precision: 30, scale: 20
+    t.float    "base_rent"
+    t.float    "bedroom_coefficient"
+    t.float    "bathroom_coefficient"
+    t.float    "sqft_coefficient"
+    t.float    "elevation_coefficient"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "area_name"
-    t.boolean  "active"
-    t.decimal  "dist_to_park_coefficient", precision: 30, scale: 20
-    t.decimal  "level_coefficient",        precision: 30, scale: 20
-    t.decimal  "age_coefficient",          precision: 30, scale: 20
-    t.decimal  "garage_coefficient",       precision: 30, scale: 20
-    t.decimal  "mser",                     precision: 30, scale: 20
   end
 
   create_table "prediction_neighborhoods", force: true do |t|
     t.integer  "prediction_model_id"
-    t.string   "name"
+    t.string   "prediction_neighborhood_name"
+    t.float    "prediction_neighborhood_coefficient"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "neighborhood_id"
-    t.boolean  "active",                                        default: false
-    t.decimal  "regular_coefficient", precision: 30, scale: 20
-    t.decimal  "luxury_coefficient",  precision: 30, scale: 20
   end
 
   create_table "prediction_results", force: true do |t|
@@ -161,13 +102,8 @@ ActiveRecord::Schema.define(version: 20151211212454) do
     t.datetime "updated_at"
     t.float    "error_level"
     t.float    "listed_rent"
-    t.string   "transaction_type",                                      default: "rental"
+    t.string   "transaction_type",    default: "rental"
     t.float    "listed_sale"
-    t.integer  "property_transaction_log_id"
-    t.float    "cap_rate"
-    t.decimal  "pred_std",                    precision: 30, scale: 20
-    t.decimal  "interval_l",                  precision: 30, scale: 20
-    t.decimal  "interval_u",                  precision: 30, scale: 20
   end
 
   create_table "properties", force: true do |t|
@@ -184,12 +120,6 @@ ActiveRecord::Schema.define(version: 20151211212454) do
     t.float    "longitude"
     t.float    "elevation"
     t.text     "lookup_address"
-    t.boolean  "luxurious"
-    t.boolean  "garage"
-    t.integer  "year_built"
-    t.integer  "level",          default: 1
-    t.float    "dist_to_park"
-    t.boolean  "sfh",            default: false
   end
 
   create_table "property_neighborhoods", force: true do |t|
